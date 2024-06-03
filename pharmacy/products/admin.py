771cc91aefac37ab05f240c11b6fb9d2dc67fa24
Edgar Mendes <mendesbr92@gmail.com>
2024-06-03 18:55:52 -0300

    Database Modeling - Created models for Product #4

--- pharmacy/products/admin.py
+++ pharmacy/products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Products
+
 
 # Register your models here.
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price')
+    search_fields = ('name', 'description', 'price')
+
+
+admin.site.register(Products, ProductsAdmin)
--- pharmacy/products/models.py
+++ pharmacy/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    img = models.ImageField(upload_to='images/products', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
